502af40e8dcb57b1deb173c22e0e95ebb383f96d
Msuf123 <akshatmalik18t@gmail.com>
2024-05-13 23:57:58 +0530

    Learnt about slicing

--- index.py
+++ index.py
@@ -1,3 +1,11 @@
 import numpy as np
 multi_d=np.array([[1,2,3],[3,4,4]])
 print(multi_d.ndim)
+#Maths operations
+array_one=np.array([1,2,3,4,5])
+array_two=np.array([4,5,6])
+# additon=array_one+array_two
+# print(additon)
+# print(array_one-array_two)
+# print(array_one*array_two)
+print(array_one[0::2])
