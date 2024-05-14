3ef58f240fd07f1a3e5dc8003bfaaf5ac8b0b5ee
Msuf123 <akshatmalik18t@gmail.com>
2024-05-14 21:29:35 +0530

    Understood hstack and hsplit

--- index.py
+++ index.py
@@ -3,10 +3,11 @@ multi_d=np.array([[1,2,3],[3,4,4]])
 print(multi_d.ndim)
 #Maths operations
 array_one=np.array([1,2,3])
-array_two=np.array([4,5,6])
+array_two=np.array([4,5,6,7,8])
 # additon=array_one+array_two
 # print(additon)
 # print(array_one-array_two)
 # print(array_one*array_two)
-print(np.vstack((array_two,array_one)))
-
+# print(np.vstack((array_two,array_one)))
+# print(np.hstack((array_two,array_one)))
+print(np.hsplit(array_two,3))
