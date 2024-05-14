69e9fcea2f7f03251f9e911bdcbf1a6ca0eb65f5
Msuf123 <akshatmalik18t@gmail.com>
2024-05-14 21:38:31 +0530

    Working on the finding determinant of matrices

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from utils import plot_lines
 multi_d=np.array([[1,2,3],[3,4,4]])
 print(multi_d.ndim)
 #Maths operations
 array_one=np.array([1,2,3])
-array_two=np.array([4,5,6,7,8])
+array_two=np.array([4,5,6,7,8,9])
 # additon=array_one+array_two
 # print(additon)
 # print(array_one-array_two)
