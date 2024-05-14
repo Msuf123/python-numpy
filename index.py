43b3106cd8148e3c6e75318ecd40e55877e295c5
Msuf123 <akshatmalik18t@gmail.com>
2024-05-14 21:20:21 +0530

    Started learing vstack

--- index.py
+++ index.py
@@ -2,10 +2,10 @@ import numpy as np
 multi_d=np.array([[1,2,3],[3,4,4]])
 print(multi_d.ndim)
 #Maths operations
-array_one=np.array([1,2,3,4,5])
+array_one=np.array([1,2,3])
 array_two=np.array([4,5,6])
 # additon=array_one+array_two
 # print(additon)
 # print(array_one-array_two)
 # print(array_one*array_two)
-print(array_one[-3:])
+print(np.vstack(array_one,array_two))
