776335950a1df1e274b6d7831158989a0a88ab8f
Msuf123 <akshatmalik18t@gmail.com>
2024-05-16 17:47:22 +0530

    Understood about the dtype

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-array=np.array([[4,1],[5,1]])
+array=np.array([[4,1],[5,1]],dtype=np.dtype(float))
+
+print(array)
 constants=np.array([10,12])
 print(np.linalg.solve(array,constants))
