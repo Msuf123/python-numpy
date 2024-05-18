9abe25eb948a7b75d14fc3ecd969c36ff0e41317
Msuf123 <akshatmalik18t@gmail.com>
2024-05-18 20:38:12 +0530

    finishing the assignment

--- index.py
+++ index.py
@@ -1,7 +1,104 @@
 import numpy as np
-import matplotlib.pyplot as plt
-array=np.array([[4,1],[5,1]],dtype=np.dtype(float))
 
-print(array)
-constants=np.array([10,12])
-print(np.linalg.solve(array,constants))
+
+def swap_rows(M, row_index_1, row_index_2):
+    """
+    Swap rows in the given matrix.
+
+    Parameters:
+    - matrix (numpy.array): The input matrix to perform row swaps on.
+    - row_index_1 (int): Index of the first row to be swapped.
+    - row_index_2 (int): Index of the second row to be swapped.
+    """
+
+    # Copy matrix M so the changes do not affect the original matrix.
+    M = M.copy()
+    # Swap indexes
+    M[[row_index_1, row_index_2]] = M[[row_index_2, row_index_1]]
+    return M
+def get_index_first_non_zero_value_from_column(M, column, starting_row):
+    """
+    Retrieve the index of the first non-zero value in a specified column of the given matrix.
+
+    Parameters:
+    - matrix (numpy.array): The input matrix to search for non-zero values.
+    - column (int): The index of the column to search.
+    - starting_row (int): The starting row index for the search.
+
+    Returns:
+    int: The index of the first non-zero value in the specified column, starting from the given row.
+                Returns -1 if no non-zero value is found.
+    """
+    # Get the column array starting from the specified row
+    column_array = M[starting_row:,column]
+    for i, val in enumerate(column_array):
+        # Iterate over every value in the column array.
+        # To check for non-zero values, you must always use np.isclose instead of doing "val == 0".
+        if not np.isclose(val, 0, atol = 1e-5):
+            # If one non zero value is found, then adjust the index to match the correct index in the matrix and return it.
+            index = i + starting_row
+            return index
+    # If no non-zero value is found below it, return -1.
+    return -1
+N = np.array([
+[0, 5, -3 ,6 ,8],
+[0, 6, 3, 8, 1],
+[0, 0, 0, 0, 0],
+[0, 0, 0 ,0 ,7],
+[0, 2, 1, 0, 4]
+]
+)
+print(N)
+print(get_index_first_non_zero_value_from_column(N, column = 4, starting_row = 2))
+
+
+def get_index_first_non_zero_value_from_row(M, row, augmented=False):
+    """
+    Find the index of the first non-zero value in the specified row of the given matrix.
+
+    Parameters:
+    - matrix (numpy.array): The input matrix to search for non-zero values.
+    - row (int): The index of the row to search.
+    - augmented (bool): Pass this True if you are dealing with an augmented matrix,
+                        so it will ignore the constant values (the last column in the augmented matrix).
+
+    Returns:
+    int: The index of the first non-zero value in the specified row.
+                Returns -1 if no non-zero value is found.
+    """
+
+    # Create a copy to avoid modifying the original matrix
+    M = M.copy()
+
+    # If it is an augmented matrix, then ignore the constant values
+    if augmented == True:
+        # Isolating the coefficient matrix (removing the constant terms)
+        M = M[:, :-1]
+
+    # Get the desired row
+    row_array = M[row]
+    for i, val in enumerate(row_array):
+        # If finds a non zero value, returns the index. Otherwise returns -1.
+        if not np.isclose(val, 0, atol=1e-5):
+            return i
+    return -1
+def augmented_matrix(A, B):
+    """
+    Create an augmented matrix by horizontally stacking two matrices A and B.
+
+    Parameters:
+    - A (numpy.array): First matrix.
+    - B (numpy.array): Second matrix.
+
+    Returns:
+    - numpy.array: Augmented matrix obtained by horizontally stacking A and B.
+    """
+    augmented_M = np.hstack((A,B))
+    return augmented_M
+A = np.array([[1,2,3], [3,4,5], [4,5,6]])
+B = np.array([[1], [5], [7]])
+
+arrayOne=np.array([[1,2],[1,2]])
+arrayTwo=np.array([[3,4],[0,4]])
+print(np.hstack((arrayOne,arrayTwo)))
+print(A[0,0])
